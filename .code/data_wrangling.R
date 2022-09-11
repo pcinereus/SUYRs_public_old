db6628fd1c519d712d22e31816e9fd5c80ea7bec
Murray Logan <i.obesulus@gmail.com>
2022-09-11 20:49:53 +1000

    pre CoE2022

--- .code/data_wrangling.R
+++ .code/data_wrangling.R
@@ -8,13 +8,26 @@ library(tidyverse) #for data wrangling
 
 ## ----getData, results='markdown', eval=TRUE-----------------------------------
 load(file='../data/manipulationDatasets.RData')
+
+
+## ----getData1, results='markdown', eval=TRUE----------------------------------
 dat.1 %>% head
 
 
-## ---- results='markup'--------------------------------------------------------
+## ----exploringdata, results='markdown', eval=TRUE-----------------------------
+## replace this with code to explore imported data
+## From now on I will not provide many code chunks.
+## Instead you are incouraged to create your own chunks
+## with discussed code
+## In Rstudio, you can create a chunk with Cntr-Alt-I
+
+
+## ----piping, results='markup'-------------------------------------------------
 head(dat.1)
 #OR
 dat.1 %>% head()
 #OR
 dat.1 %>% head
+#OR
+dat.1 |> head()
 
